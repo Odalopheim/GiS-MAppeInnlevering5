4a580c1e9911a70f17fb87a8b02469973805cacd
Odalopheim <odalo@uia.no>
2025-05-07 11:16:30 +0200

    endret så denn finner navnet på hyttene

--- dnt_hytter.js
+++ dnt_hytter.js
@@ -2,5 +2,5 @@
 import { fetchGeoJSONPunkter} from './punkter.js'; // Importer én gang
 
 export async function fetchGeoJSONHytter(map, layerGroup) {
-    await fetchGeoJSONPunkter(map, layerGroup, 'DNT_Hytter_view', '#0074D9');
+    await fetchGeoJSONPunkter(map, layerGroup, 'dnt_hytter_view', '#0074D9');
 }
